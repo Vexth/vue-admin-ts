79bf0527814f7336820971d0cca8107e90dc39e7
ouyc <oo932758@sina.com>
2019-11-25 10:29:08 +0800

    m

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,29 +12,6 @@ import nestedRouter from './modules/nested'
 
 Vue.use(Router)
 
-/*
-  Note: sub-menu only appear when children.length>=1
-  Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
-*/
-
-/*
-  name:'router-name'             the name field is required when using <keep-alive>, it should also match its component's name property
-                                 detail see : https://vuejs.org/v2/guide/components-dynamic-async.html#keep-alive-with-Dynamic-Components
-  redirect:                      if set to 'noredirect', no redirect action will be trigger when clicking the breadcrumb
-  meta: {
-    roles: ['admin', 'editor']   will control the page roles (allow setting multiple roles)
-    title: 'title'               the name showed in subMenu and breadcrumb (recommend set)
-    icon: 'svg-name'             the icon showed in the sidebar
-    hidden: true                 if true, this route will not show in the sidebar (default is false)
-    alwaysShow: true             if true, will always show the root menu (default is false)
-                                 if false, hide the root menu when has less or equal than one children route
-    breadcrumb: false            if false, the item will be hidden in breadcrumb (default is true)
-    noCache: true                if true, the page will not be cached (default is false)
-    affix: true                  if true, the tag will affix in the tags-view
-    activeMenu: '/example/list'  if set path, the sidebar will highlight the path you set
-  }
-*/
-
 /**
   ConstantRoutes
   a base page that does not have permission requirements
@@ -89,18 +66,6 @@ export const constantRoutes: RouteConfig[] = [
       }
     ]
   },
-  // {
-  //   path: '/documentation',
-  //   component: Layout,
-  //   children: [
-  //     {
-  //       path: 'index',
-  //       component: () => import(/* webpackChunkName: "documentation" */ '@/views/documentation/index.vue'),
-  //       name: 'Documentation',
-  //       meta: { title: 'documentation', icon: 'documentation', affix: true }
-  //     }
-  //   ]
-  // },
   {
     path: '/guide',
     component: Layout,
