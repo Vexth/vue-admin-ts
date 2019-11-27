a56272095142f5ef46f92d98bef24a872f77bcfe
ouyc <oo932758@sina.com>
2019-11-27 08:26:10 +0800

    m

--- src/utils/get-page-title.ts
+++ src/utils/get-page-title.ts
@@ -1,6 +1,6 @@
 import defaultSettings from '@/settings'
 
-const title = defaultSettings.title || 'Vue Element Admin'
+const title = defaultSettings.title || 'Vue Typescript Admin'
 
 export default function getPageTitle(pageTitle: any) {
   if (pageTitle) {
